5ed3d2880349f4b16d04af7a99a979a4fb0d0551
Oleksii Onysymchuk <Oleksii_Onysymchuk@epam.com>
2017-03-15 16:59:20 +0200

    first commit

---
+++ src/main/java/soccer/Game.java
@@ -0,0 +1,123 @@
+package soccer;
+
+/**
+ * Created by Oleksii_Onysymchuk on 3/14/2017.
+ */
+public class Game {
+    private String firstTeamName;
+    private String secondTeamName;
+    private int strikesCount = 0;
+    private int firstTeamScore = 0;
+    private int secondTeamScore = 0;
+    private int firstTeamFailedPenaltiesPlayersCost;
+    private int secondTeamFailedPenaltiesPlayersCost;
+
+    public Game() {
+    }
+
+    public Game(String firstTeamName, String secondTeamName) {
+        this.firstTeamName = firstTeamName;
+        this.secondTeamName = secondTeamName;
+    }
+
+    public boolean[] teamShotSuccessful(int teamNumber, boolean isSuccessful, String playerName) {
+//        if (isFinished()){
+//            throw new IllegalStateException("The game is OVER!");
+//        }
+        strikesCount++;
+        addPlayerStatistics(playerName, isSuccessful);
+        if (isSuccessful) {
+            if (teamNumber == 1) {
+                firstTeamScore++;
+            } else {
+                secondTeamScore++;
+            }
+        } else {
+            if (teamNumber == 1) {
+                firstTeamFailedPenaltiesPlayersCost += getPlayerCostByName(playerName);
+            } else {
+                secondTeamFailedPenaltiesPlayersCost += getPlayerCostByName(playerName);
+            }
+        }
+        return getPlayersStatistics(playerName);
+    }
+
+    protected int getPlayerCostByName(String playerName) {
+        return 0;
+    }
+
+    protected void addPlayerStatistics(String playerName, boolean isSuccessful) {
+
+    }
+
+    protected boolean[] getPlayersStatistics(String playerName) {
+        return new boolean[10];
+    }
+
+    public String score() {
+        String firstPart = "(" + firstTeamScore + ") " + firstTeamName;
+        String secondPart = secondTeamName + " (" + secondTeamScore + ")";
+        if (strikesCount / 2 >= 7) {
+            firstPart = firstPart + " [" + firstTeamFailedPenaltiesPlayersCost + "]";
+            secondPart = secondPart + " [" + secondTeamFailedPenaltiesPlayersCost + "]";
+        }
+        return firstPart + " : " + secondPart;
+    }
+
+    public boolean isFinished() {
+        if ((strikesCount < 10) && isPairOfShotsFull()) {
+            if (scoreDifference() > strikesTo5()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if ((strikesCount == 10)) {
+            if (isEqualScore()) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        if (isAdditionalShots()) {
+            if (!isPairOfShotsFull()) {
+                return false;
+            } else {
+                if (isEqualScore()) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int strikesTo5() {
+        return 5 - strikesCount / 2;
+    }
+
+    public int scoreDifference() {
+        return Math.abs(firstTeamScore - secondTeamScore);
+    }
+
+    public boolean isAdditionalShots() {
+        return strikesCount > 10;
+    }
+
+    public boolean isPairOfShotsFull() {
+        return strikesCount % 2 == 0;
+    }
+
+    private boolean isEqualScore() {
+        return firstTeamScore == secondTeamScore;
+    }
+
+    public void setFirstTeamName(String firstTeamName) {
+        this.firstTeamName = firstTeamName;
+    }
+
+    public void setSecondTeamName(String secondTeamName) {
+        this.secondTeamName = secondTeamName;
+    }
+}
---
+++ src/test/java/soccer/GameTest.java
@@ -0,0 +1,140 @@
+package soccer;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GameTest {
+    @Spy
+    private Game game;
+    private String firstTeamName = "Metallist";
+    private String secondTeamName = "Arsenal";
+
+    @Before
+    public void setUp() {
+        game.setFirstTeamName(firstTeamName);
+        game.setSecondTeamName(secondTeamName);
+    }
+
+    @Test
+    public void testScoreIs10() {
+        game.teamShotSuccessful(1, true, null);
+        game.teamShotSuccessful(2, false, null);
+        assertThat(game.score(), is("(1) Metallist : Arsenal (0)"));
+    }
+
+    @Test
+    public void testScoreIs01() {
+        game.teamShotSuccessful(1, false, null);
+        game.teamShotSuccessful(2, true, null);
+        assertThat(game.score(), is("(0) Metallist : Arsenal (1)"));
+    }
+
+    @Test
+    public void testScoreIs43() {
+        bothTeamShots(3, true);
+        firstTeamShots(1, true);
+        assertThat(game.score(), is("(4) Metallist : Arsenal (3)"));
+    }
+
+    @Test
+    public void testIsNotFinishedJustAfterStart() {
+        assertThat(game.isFinished(), is(false));
+        game.setFirstTeamName(firstTeamName);
+        game.setSecondTeamName(secondTeamName);
+    }
+
+    @Test
+    public void testIsFinishedAfter10ShotsAndScore54() {
+        firstTeamShots(5, true);
+        secondTeamShots(4, true);
+        secondTeamShots(1, false);
+        assertThat(game.isFinished(), is(true));
+    }
+
+    @Test
+    public void testIsFinishedAfter10ShotsAndScore55() {
+        firstTeamShots(5, true);
+        secondTeamShots(5, true);
+        assertThat(game.isFinished(), is(false));
+    }
+
+    @Test
+    public void testIsNotFinishedAfter9ShotsAndScore50() {
+        firstTeamShots(1, true);
+        bothTeamShots(4, true);
+        assertThat(game.isFinished(), is(false));
+    }
+
+    @Test
+    public void testIsNotFinishedAfter11ShotsAndScore65() {
+        firstTeamShots(1, true);
+        bothTeamShots(5, true);
+        assertThat(game.isFinished(), is(false));
+    }
+
+    @Test
+    public void testIsFinishedAfter12ShotsAndScore65() {
+        bothTeamShots(5, true);
+        secondTeamShots(1, false);
+        assertThat(game.isFinished(), is(true));
+    }
+
+    @Test
+    public void testIsFinishedAfter6ShotsAndScore30() {
+        firstTeamShots(3, true);
+        secondTeamShots(3, false);
+        assertThat(game.isFinished(), is(true));
+    }
+
+    @Test
+    public void testReturnsStatAfterShot() {
+        boolean[] shotsHistory = new boolean[]{true, false, true, true, true, true, true, true, true, false};
+        when(game.getPlayersStatistics("Player1"))
+                .thenReturn(shotsHistory);
+        assertThat(game.teamShotSuccessful(1, false, "Player1"), is(shotsHistory));
+    }
+
+    @Test
+    public void testDoNotShowsFailedPlayersSumAfter6Series() {
+        firstTeamShots(6, false);
+        secondTeamShots(6, false);
+        assertThat(game.score(), is("(0) Metallist : Arsenal (0)"));
+    }
+
+    @Test
+    public void testShowsFailedPlayersSumAfter7Series() {
+        when(game.getPlayerCostByName(anyString())).thenReturn(10);
+        firstTeamShots(7, false);
+        secondTeamShots(7, false);
+        assertThat(game.score(), is("(0) Metallist [70] : Arsenal (0) [70]"));
+    }
+
+    private void firstTeamShots(int quantity, boolean isSuccessful) {
+        for (int i = 0; i < quantity; i++) {
+            game.teamShotSuccessful(1, isSuccessful, null);
+        }
+    }
+
+    private void secondTeamShots(int quantity, boolean isSuccessful) {
+        for (int i = 0; i < quantity; i++) {
+            game.teamShotSuccessful(2, isSuccessful, null);
+        }
+    }
+    private void bothTeamShots(int quantity, boolean isSuccessful) {
+        for (int i = 0; i < quantity; i++) {
+            game.teamShotSuccessful(1, isSuccessful, null);
+            game.teamShotSuccessful(2, isSuccessful, null);
+        }
+    }
+
+
+}
